ebe6260f3fb7f0bf98c037cc84da4d9b33e8f0ac
NikitaBelikov <belsta.nik@gmail.com>
2025-01-07 22:00:55 +0300

    Add func main

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var addLetters bool
+var addNumbers bool
+var addCharacters bool
+var lenghtString int
+
+func main() {
+	
+	fmt.Println("Welcome to the Password Generator!")
+
+
+}
